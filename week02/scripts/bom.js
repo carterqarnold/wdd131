c884bf8ef9ce430cb3c8ce8663544af064b45075
CarterQArnold <arn24013@byui.edu>
2025-01-17 13:43:20 -0700

    BOM activity working better but not done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,24 +1,23 @@
-
-
+const submitButton = document.querySelector("button");
+const typeElement = document.querySelector("input");
+const list = document.querySelector("#list");
+        
+const liElement = document.createElement("li");
+const deleteButton = document.createElement("button");
 submitButton.addEventListener(click, function() {
-    if (typeElement.value.trim() != "")
+
+    if (typeElement.value.trim() !== "")
     {
-    const submitButton = document.querySelector("button");
-    const typeElement = document.querySelector("input");
-    const list = document.querySelector("list");
-    
-    const liElement = document.createElement("li");
-    const deleteButton = document.createElement("button");
-    
-    liElement.textContent = typeElement.value;
-    deleteButton.textContent = "❌";
-    
-    liElement.append(deleteButton);
-    list.append(liElement);
+        liElement.textContent = typeElement.value;
+        deleteButton.textContent = "❌";
+        
+        liElement.append(deleteButton);
+        list.append(liElement);
     }
     
     deleteButton.addEventListener(click,function()
     {
+        list.removeChild(liElement);
         typeElement.focus();
     });
     typeElement.value = "";
