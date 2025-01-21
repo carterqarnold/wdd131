b35abaa722ea00c843b6070af84a33fc971d68b5
CarterQArnold <arn24013@byui.edu>
2025-01-20 17:17:32 -0700

    need some help with the hamburger button working

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,18 +1,8 @@
 
-const hamButton = document.querySelector("button");
-const navigation = document.querySelector("nav");
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
 hamButton.addEventListener("click", () => {
-    navigation.classList.toggle("show");
-    hamButton.classList.toggle("show");
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
 });
-
-const year = document.querySelector("#currentyear");
-const today = new Date();
-const lastModifiedDate = new Date(document.lastModified);
-
-const formattedDate = lastModifiedDate.toLocaleString();
-
-year.innerHTML = `<span>${today.getFullYear()}</span>`;
-const elementToUpdate = document.getElementById("lastModified");
-elementToUpdate.innerHTML = `Last Modification: ${formattedDate}`;
