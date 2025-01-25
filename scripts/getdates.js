be76bd127b295691da2916f71f47a944baa3f508
CarterQArnold <arn24013@byui.edu>
2025-01-25 02:46:24 -0700

    update

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,8 +1,6 @@
-
 const year = document.querySelector("#currentyear");
 const today = new Date();
 const lastModifiedDate = new Date(document.lastModified);
-
 const formattedDate = lastModifiedDate.toLocaleString();
 
 year.innerHTML = `<span>${today.getFullYear()}</span>`;
