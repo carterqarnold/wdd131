b40a6113f5a1362cbc577d87a462a6ebff7b4c06
CarterQArnold <arn24013@byui.edu>
2025-02-15 21:49:21 -0700

    update

---
+++ project/scripts/dance.js
@@ -0,0 +1,11 @@
+let today = new Date();
+document.querySelector("#currentyear").innerHTML = `&copy;${today.getFullYear()}`;
+document.getElementById("lastModified").textContent = `Last Modification: ${document.lastModified}`;
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+})
